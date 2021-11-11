59315f5d23c9fb60ec1443fb48a6a9276093b1a8
JayTucker2 <prosnipa1221@gmail.com>
2021-11-11 08:08:36 -0600

    Initial Commit

---
+++ RockPaperSckizzor/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RockPaperSckizzor
+//
+//  Created by JAYLAN TUCKER on 11/9/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var rock = 0
+    var paper = 1
+    var scissors = 2
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func fightBut(_ sender: Any) {
+        
+    }
+    
+
+}
+
