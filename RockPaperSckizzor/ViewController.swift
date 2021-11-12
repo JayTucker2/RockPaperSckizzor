c74e8d845646ff860d9228caa149c9907ad967d8
JayTucker2 <prosnipa1221@gmail.com>
2021-11-12 08:05:46 -0600

    Secondary Commit

--- RockPaperSckizzor/ViewController.swift
+++ RockPaperSckizzor/ViewController.swift
@@ -11,15 +11,31 @@ class ViewController: UIViewController {
     var rock = 0
     var paper = 1
     var scissors = 2
+    func bot()-> Int{
+        let botRes = Int.random(in: 0...2)
+        return botRes
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-    @IBAction func fightBut(_ sender: Any) {
-        
+    @IBAction func rockBut(_ sender: Any) {
+    var resp = bot()
+        if resp == 0{
+            
+        }
+            
+        }
+    
+    
+    @IBAction func paperBut(_ sender: Any) {
     }
     
+    @IBAction func scisBut(_ sender: Any) {
+    }
 
-}
+    
+
+
 
