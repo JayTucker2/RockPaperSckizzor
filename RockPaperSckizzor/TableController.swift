1e232175864efdd5cb4a5d7900b3e958ffaa4b5a
JayTucker2 <prosnipa1221@gmail.com>
2021-11-19 08:08:38 -0600

    progress report

---
+++ RockPaperSckizzor/TableController.swift
@@ -0,0 +1,29 @@
+//
+//  TableController.swift
+//  RockPaperSckizzor
+//
+//  Created by JAYLAN TUCKER on 11/19/21.
+//
+
+import UIKit
+
+class TableController: UIViewController, UITableViewDelegate, UITableViewDataSource{
+    
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+
+   
+
+}
